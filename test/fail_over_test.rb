e09bd85c7bcd82ba6d47201e90b24bf1f6c4a49f
Charles Ng <charles.ng@shopify.com>
2014-11-03 14:12:17 -0500

    Add Redis helper functions

--- test/fail_over_test.rb
+++ test/fail_over_test.rb
@@ -7,4 +7,31 @@ class FailOverTest < MiniTest::Test
   def test_initial
     assert_equal "redis1", check_host
   end
+
+  def test_redis_up
+    assert check_redis_up?
+  end
+
+  def test_redis_key_set
+    begin
+      assert_equal 'OK', redis_set('foo', 'bar')
+    ensure
+      redis_flushdb
+    end
+  end
+
+  def test_redis_key_get
+    begin
+      redis_set('foo', 'bar')
+      assert_equal 'bar', redis_get('foo')
+    ensure
+      redis_flushdb
+    end
+  end
+
+  def test_redis_flushdb
+    redis_set('foo', 'bar')
+    redis_flushdb
+    assert_nil redis_get('foo')
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ require 'minitest'
 require 'net/http'
 require 'pry'
 require 'minitest/autorun'
+require 'redis'
 
 def vip_address
   "172.28.33.15"
@@ -13,4 +14,24 @@ end
 
 def check_host
   Net::HTTP.get(URI.parse("http://#{vip_address}")).chomp
+end
+
+def redis_client
+  @redis = @redis || Redis.new(:host => vip_address, :port => redis_port)
+end
+
+def check_redis_up?
+  redis_client.ping == 'PONG'
+end
+
+def redis_set(key, value)
+  redis_client.set(key, value)
+end
+
+def redis_get(key)
+  redis_client.get(key)
+end
+
+def redis_flushdb()
+  redis_client.flushdb
 end
